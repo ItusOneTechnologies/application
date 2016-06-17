3ddbfb9d3ddc299ee48946d87ec0021a9a026be5
treydecker75 <treydecker75@gmail.com>
2016-06-16 20:58:54 -0400

    Add example chart for report

--- app/components/report/reportControllers.js
+++ app/components/report/reportControllers.js
@@ -6,5 +6,25 @@ app.directive("reports", function() {
   }
 });
 app.controller("reportController", function($scope) {
-  
-})
+  angular.element(document).ready( function() {
+    console.log('this text worked');
+    var hdata = {
+      labels: ["16:47", "17:47", "18:47", "19:47", "20:47"],
+      datasets: [
+        {
+          label: "Heartrate by the Hour",
+          backgroundColor: "rgba(172,194,132,0.4)",
+          strokeColor: "#ACC26D",
+          pointBackgroundColor: "#fff",
+          pointBorderColor: "#9DB86D",
+          data: [66, 77, 108, 85, 75]
+        }
+      ] 
+    }
+    var heartrate = document.getElementById('heartrate').getContext('2d');
+    var chart = new Chart(heartrate, {
+      type: 'line',
+      data: hdata
+    });
+  });
+});
