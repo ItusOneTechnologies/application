3bdbcd9ccdfe994fe23da503cc08c5042d0f70d2
treydecker75 <treydecker75@gmail.com>
2016-06-23 20:23:17 -0400

    Change font, color, and make sidebar scope manage own interaction

--- app/shared/sidebar/sidebarDirective.js
+++ app/shared/sidebar/sidebarDirective.js
@@ -1,7 +1,36 @@
 app.directive("sidebar", function() {
   return {
+//     scope: {},
+    transclude: true, 
     restrict: 'E',
     replace: true,
-    templateUrl: './app/shared/sidebar/sidebarView.html'
+    templateUrl: './app/shared/sidebar/sidebarView.html',
+    controller: ['$scope', function($scope) {
+      $scope.sidebar = $('#sidebar');
+      $scope.test = function() {
+        console.log('testing. . .');
+      }
+      $scope.slideOut = function() {
+        var wrapper = $('#wrapper');
+        wrapper.animate({
+          'left': '0'
+        }, 1000);
+        $scope.sidebar.animate({
+          'left': '-250px'
+        }, 1000);
+        $scope.sidebar.removeClass('open');
+      };
+      $scope.slideIn = function() {
+        var wrapper = $('#wrapper');
+        wrapper.animate({
+          'left': '250px'
+        }, 1000);
+        $scope.sidebar.animate({
+          'left': '0'
+        }, 1000);
+        $scope.sidebar.addClass('open');
+      }
+    }],
+    controllerAs: 'sidebarCtrl'
   }
 });
