5d74088292bac47abb8d0b36f5589c546ca961e9
treydecker75 <treydecker75@gmail.com>
2016-06-23 21:48:57 -0400

    Add pointer for dropdowns

--- app/shared/header/headerDirective.js
+++ app/shared/header/headerDirective.js
@@ -7,6 +7,31 @@ app.directive("appHeader", function () {
       $scope.sidebarClick = function () {
         navClicked();
       };
+      $scope.reportNavClick = function($event) {
+        var offset = $($event.currentTarget).offset();
+        var li = $('.report-nav-li');
+        var center = li.width()/2;
+        if ($('.pointer').hasClass('open')) {
+          $scope.closePointer();
+        } else {
+          $scope.openPointer(offset, center);
+        }
+      };
+      $scope.openPointer = function(offset, center) {
+        var pointer = $('.pointer');
+        pointer.css({
+          'display': 'block',
+          'left': offset.left + center
+        });
+        pointer.addClass('open');
+      };
+      $scope.closePointer = function() {
+        var pointer = $('.pointer');
+        pointer.css({
+          'display': 'none'
+        });
+        pointer.removeClass('open');
+      } 
     }
   }
 });
