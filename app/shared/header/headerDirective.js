c79e73bb4ad60779e96a4bd5fae337c201b112a5
treydecker75 <treydecker75@gmail.com>
2016-06-25 09:39:59 -0400

    Add interactivity to dropdowns

--- app/shared/header/headerDirective.js
+++ app/shared/header/headerDirective.js
@@ -4,11 +4,32 @@ app.directive("appHeader", function () {
     replace: true,
     restrict: 'E',
     controller: function ($scope) {
+      $scope.previousNav;
       $scope.sidebarClick = function () {
         navClicked();
       };
       $scope.reportNavClick = function($event) {
-        console.log($event.currentTarget);
+        var dd = $(event.currentTarget).next();
+        if ($(dd).hasClass('open-nav')) {
+          $(dd).css('display', 'none');
+          $(dd).removeClass('open-nav');
+        } else {
+          if ($scope.previousNav) {
+            $scope.previousNav.css('display', 'none');
+            $scope.previousNav.removeClass('open-nav');
+          }
+          var startHeight = 0;
+          var autoHeight = $(dd).css({
+            'display': 'block',
+            'height': 'auto'
+          }).height();
+          $(dd).height(startHeight).animate({
+            'height': autoHeight,
+            'display': 'block'
+          }, 750);
+          $(dd).addClass('open-nav');
+          $scope.previousNav = $(dd);
+        }     
       };
       $scope.openPointer = function(offset, center) {
         var pointer = $('.pointer');
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,25 @@ function navClicked() {
     sidebar.slideIn();
   }
 }
+function slideUp(element) {
+  var currHeight = element.height();
+  var noHeight = 0;
+  element.height(currHeight).animate({
+    'height': noHeight
+  }, 500, function() {
+    element.css({
+      'display': 'none'
+    });
+  });
+}
+function slideDown(element) {
+  var startHeight = 0;
+  var autoHeight = element.css({
+    'display': 'block',
+    'height': 'auto'
+  }).height();
+  element.height(startHeight).animate({
+    'height': autoHeight,
+    'display': 'block'
+  }, 750);
+}
